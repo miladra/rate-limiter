6f49073c64f17a69fbf6d20e54857d30bb7a64b1
miladra <miladranjbari@gmail.com>
2022-06-02 18:21:03 +0430

    Add unit test

---
+++ src/test/java/com/payconiq/stock/service/mapper/StockMapperTest.java
@@ -0,0 +1,15 @@
+package com.payconiq.stock.service.mapper;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class StockMapperTest {
+
+    private StockMapper stockMapper;
+
+    @BeforeEach
+    public void setUp() {
+    }
+}
